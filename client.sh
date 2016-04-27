d8f97b2ee9037a311c226b0c21f3f8cecd295303
SpeedStream <aesc91@hotmail.com>
2016-04-27 12:04:50 -0500

    V2.0

--- client.sh
+++ client.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-outPipe="outPipe"
 while :
 	do
+		#Abrimos el puerto en el IP y leemos
+		#Guardamos el valor que leemos en val
 		val=$(netcat $1 $2)
-		echo $val > outPipe
-		display $val.jpg
-		sleep 0.5
+
+		#Debido al refresh cada 0.2s, habrá momentos donde no tengamos nada
+		if [ -z $val ]; then
+			#Si $val es vacío, realizamos un echo
+			echo
+		else
+			#Si existe un valor $val, entonces realizamos el fork y mostramos con display la imagen correspondiente
+			(display -remote $val.jpg) &
+		fi
+		sleep 0.2
 	done
--- script.sh
+++ script.sh
@@ -1,25 +1,38 @@
 #!/bin/bash
 
-usegF="usegF"
-dsegF="dsegF"
-#uminF="uminF"
-#dminF="dminF"
+#Pipes a escribir
+# dmin umin : dseg useg
+#   0   0   :  0    0
+usegF="usegF"	#Unidades de segundo 	-> Pipe de Unidades de Segundo
+dsegF="dsegF"	#Decenas de segundo 	-> Pipe de Decenas de Segundo
+uminF="uminF"	#Unidades de minuto 	-> Pipe de Unidades de Minuto
+dminF="dminF"	#Decenas de minuto 		-> Pipe de Decenas de Minuto
 
 while :
 	do
+		#Obtenemos minutos y segundos
 		SEG=$(date +%S)
 		MIN=$(date +%M)
+		
+		#Separamos las unidades y decenas de cada uno
 		useg=$(expr $SEG % 10)
 		dseg=$(expr $SEG / 10)
-		#umin=$(expr $MIN % 10)
-		#dmin=$(expr $MIN / 10)
+		umin=$(expr $MIN % 10)
+		dmin=$(expr $MIN / 10)
+		
+		#Dirigimos cada valor individual al pipe correspondiente
 		echo "$useg" > ${usegF}
 		echo "$dseg" > ${dsegF}
-		#echo "$umin" > ${umin}
-		#echo "$dmin" > ${dmin}
+		echo "$umin" > ${uminF}
+		echo "$dmin" > ${dminF}
+		
+		#Abrimos un puerto con netcat
+		#Enviamos el valor correspondiente
 		netcat -l 3333 < usegF
 		netcat -l 3334 < dsegF
-		#netcat -l 3335 < uminF
-		#netcat -l 3336 < dminF
+		netcat -l 3335 < uminF
+		netcat -l 3336 < dminF
+		#Esperamos 0.5seg para volver a escribir
+		#Con esto impedimos una des-sincronización
 		sleep 0.5
 	done
